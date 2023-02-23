6d6e8ba9e4b4958084752212d119b55ed1b40ac9
heidi <heidi@DESKTOP-OHLBJMO.dhcp.wustl.edu>
2023-02-23 14:06:30 -0600

    olive!!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,15 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(25,25,0);
+		StdDraw.setPenRadius(.01);
+		StdDraw.line(.6, .85, .3, .25);
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.rectangle(.5, .5, .5, .5);
+		StdDraw.setPenColor(50,150,50);
+		StdDraw.filledEllipse(.5, .5, .175, .25);
+		StdDraw.setPenColor(200,0,50);
+		StdDraw.filledCircle(.5,.6, .1);
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -18,7 +19,45 @@ public class InterpretDrawingFile {
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
-		Scanner in = new Scanner(f); //making Scanner with a File
+		Scanner in = new Scanner(f); //making Scanner with a FileNotFoundException
+		String shape = in.next();
+		int color1 = in.nextInt();
+		int color2 = in.nextInt();
+		int color3 = in.nextInt();
+		boolean shaded = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfwidth = in.nextDouble();
+		double halfheight = in.nextDouble();
 		
+		StdDraw.setPenColor (color1, color2, color3);
+		if (shaded == true && shape.equalsIgnoreCase("rectangle")) {
+			StdDraw.filledRectangle(x,y,halfwidth,halfheight);
+		}
+		else if (shaded == false && shape.equalsIgnoreCase("rectangle")) {
+			StdDraw.rectangle(x,y,halfwidth,halfheight);
+		}
+		else if (shaded == true && shape.equalsIgnoreCase("ellipse"))
+		{
+			StdDraw.filledEllipse(x,y,halfwidth,halfheight);
+		}
+		else if (shaded == false && shape.equalsIgnoreCase("ellipse"))
+		{
+			StdDraw.ellipse(x,y,halfwidth,halfheight);
+		}
+		else if (shaded == true && shape.equalsIgnoreCase("square"))
+		{
+			StdDraw.filledSquare(x, y, halfheight);
+		}
+		else if (shaded == false && shape.equalsIgnoreCase("square"))
+		{
+			StdDraw.square(x, y, halfheight);
+		}
+		
+		
+		//StdDraw.setPenColor(Color.RED);
+		//StdDraw.filledRectangle(.5, .5, .5, .25);
+		
+		//System.out.println(in.nextLine());
 	}
 }
